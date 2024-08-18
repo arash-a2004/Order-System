3df934f74f2bbf4866e3a23dbb76cc05baaaa7d9
[Arash] <arash.univer1@gmail.com>
2024-08-18 18:53:05 +0330

    add configuration

---
+++ RestApi/RabbitSender/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitSender;
+
+//create service collection for DI
+var serviceCollection = new ServiceCollection();
+
+//Build configuration
+IConfiguration configuration;
+configuration = new  ConfigurationBuilder()
+    .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+    .AddJsonFile("appsetting.json")
+    .Build();
+
+//add configuration to service collection 
+serviceCollection.AddSingleton<IConfiguration>(configuration);
+serviceCollection.AddSingleton<UserPass>();
+
+//test
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var testInstance = serviceProvider.GetService<UserPass>();
+testInstance.GetData();
+
---
+++ RestApi/RabbitSender/UserPass.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitSender
+{
+    public class UserPass
+    {
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        private readonly IConfiguration configuration;
+
+        public UserPass(IConfiguration configuration1)
+        {
+            configuration = configuration1;
+        }
+
+        public void GetData()
+        {
+            UserName = configuration.GetSection("username").Value;
+            Password = configuration.GetSection("password").Value;
+        }
+    }
+}
