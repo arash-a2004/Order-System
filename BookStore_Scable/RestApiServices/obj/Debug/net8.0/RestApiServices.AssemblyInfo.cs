75b677f30a8cc29cb53841a8e1075437dda439eb
[Arash] <arash.univer1@gmail.com>
2024-09-15 10:42:22 +0330

    docker: override docker file

--- BookStore_Scable/RestApiServices/obj/Debug/net8.0/RestApiServices.AssemblyInfo.cs
+++ BookStore_Scable/RestApiServices/obj/Debug/net8.0/RestApiServices.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("4a10dd62-c12c-4797-90fe-859b7708c13b")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestApiServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d083e6d2738135b039d86560848fbe34c83d9ee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eef5ff1e4660be5b29b861dc9a455f2aa7377ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestApiServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestApiServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
